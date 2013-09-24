53b9eff3ffc1768d3503021e7248351e24d59af7
Byron Ruth <b@devel.io>
2013-09-24 09:41:04 -0400

    Fix test http server, change to echo back request body

--- tests/httpd.py
+++ tests/httpd.py
@@ -1,10 +1,14 @@
-import SimpleHTTPServer
 import BaseHTTPServer
 
-class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def do_POST(s):
-        s.send_response(200)
-        s.end_headers()
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_type = self.headers.getheader('content-type')
+        content_length = int(self.headers.getheader('content-length'))
+        self.send_response(200)
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', str(content_length))
+        self.end_headers()
+        self.wfile.write(self.rfile.read(content_length))
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
