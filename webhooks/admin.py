bff010be0ee1a8e512486777c47228449a766cd3
Byron Ruth <b@devel.io>
2013-09-24 09:47:44 -0400

    Add custom ModelAdmin for easier editing from list view

--- webhooks/admin.py
+++ webhooks/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Webhook
 
-admin.site.register(Webhook)
+class WebhookAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'event', 'url')
+    list_editable = ('event', 'url')
+    list_filter = ('event',)
+
+admin.site.register(Webhook, WebhookAdmin)
